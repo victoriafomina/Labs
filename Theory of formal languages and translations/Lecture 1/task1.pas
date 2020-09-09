b45d41355bb5cdc0193c130ba09bf0446ca0ef8f
Victoria <viky.fomina@yandex.ru>
2020-09-09 20:49:58 +0300

    adds

---
+++ Theory of formal languages and translations/Lecture 1/task1.pas
@@ -0,0 +1,22 @@
+﻿var
+  i, j, k, numberOfElementsInTriangle, numberOfLevels : integer;
+  
+begin
+  readln(k);
+  
+  numberOfLevels := 0;
+  
+  while numberOfLevels * (numberOfLevels + 1) div 2 < k do
+    numberOfLevels += 1;
+    
+  numberOfLevels -= 1;
+  
+  numberOfElementsInTriangle := numberOfLevels * (numberOfLevels + 1) div 2;
+  
+  j := k - numberOfElementsInTriangle;
+  i := j - numberOfLevels - 2;
+  
+  writeln('i = ', i);
+  writeln('j = ', j);
+
+end.
