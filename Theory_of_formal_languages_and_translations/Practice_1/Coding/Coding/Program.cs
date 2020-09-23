ff38b1cc662747c19fa00d2242ef57afd19c48e1
Victoria <viky.fomina@yandex.ru>
2020-09-23 12:08:44 +0300

    colored console

--- Theory_of_formal_languages_and_translations/Practice_1/Coding/Coding/Program.cs
+++ Theory_of_formal_languages_and_translations/Practice_1/Coding/Coding/Program.cs
@@ -15,8 +15,18 @@ namespace Coding
                 text = reader.ReadToEnd();
             }
 
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Text:");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(text);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nCodes:");
+            Console.ForegroundColor = ConsoleColor.Yellow;
             CodingLogic coding = new CodingLogic(text);
             coding.Run();
+
+            Console.ResetColor();
         }
     }
 }
