69f9310dbab444a9ab1d7d724cbdc06f6d74bff0
Victoria <viky.fomina@yandex.ru>
2020-09-23 18:33:08 +0300

    alphabetical index is ready

---
+++ Theory_of_formal_languages_and_translations/Tests/AlphabeticalIndex/AlphabeticalIndex/AlphabeticalIndex.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlphabeticalIndex
+{
+    public class AlphabeticalIndex
+    {
+        private string text;
+
+        private List<string> splitedText;
+
+        private char[] separators = { ' ', '\t', '\r', '.', ',', ';', '-', '−', '–', '—' };
+
+        private List<(string, List<int>)> alphabeticalIndex;
+
+        public AlphabeticalIndex(string text)
+        {
+            this.text = text.ToLower();
+            this.splitedText = new List<string>();
+            this.alphabeticalIndex = new List<(string, List<int>)>();
+            SplitText();
+            CreateAlphabeticalIndex();
+            alphabeticalIndex.Sort();
+        }
+
+        private void SplitText()
+        {
+            string[] splitedTextFirstVersion = text.Split(separators);
+
+            for (var i = 0; i < splitedTextFirstVersion.Length; ++i)
+            {
+                if (splitedTextFirstVersion[i] != "" && splitedTextFirstVersion[i][0] != '\n')
+                {
+                    splitedText.Add(splitedTextFirstVersion[i]);
+                }
+                else if (splitedTextFirstVersion[i] != "" && splitedTextFirstVersion[i][0] == '\n')
+                {
+                    splitedText.Add("\n");
+
+                    if (splitedTextFirstVersion[i].Length > 1)
+                    {                        
+                        splitedText.Add(splitedTextFirstVersion[i].Substring(1));
+                    }
+                }
+            }
+        }
+
+        public List<(string, List<int>)> GetAlphabeticalIndex() => alphabeticalIndex;
+
+        private void CreateAlphabeticalIndex()
+        {
+            int stringNumber = 1;
+
+            for (var i = 0; i < splitedText.Count; ++i)
+            {
+                if (splitedText[i] == "\n")
+                {
+                    ++stringNumber;
+                }
+                else if (WordExistsInAlphabeticalIndex(splitedText[i]))
+                {
+                    int wordIndex = WordIndexInAlphabeticalIndex(splitedText[i]);
+                    alphabeticalIndex[wordIndex].Item2.Add(stringNumber);
+                }
+                else
+                {
+                    alphabeticalIndex.Add((splitedText[i], new List<int> { stringNumber }));
+                }
+            }
+        }
+
+        private int WordIndexInAlphabeticalIndex(string word)
+        {
+            int index = -1;
+
+            for (var i = 0; i < alphabeticalIndex.Count; ++i)
+            {
+                if (alphabeticalIndex[i].Item1 == word)
+                {
+                    index = i;
+                    return index;
+                }
+            }
+
+            return index;
+        }
+
+        private bool WordExistsInAlphabeticalIndex(string word)
+        {
+            foreach (var wrd in alphabeticalIndex)
+            {
+                if (wrd.Item1 == word)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Theory_of_formal_languages_and_translations/Tests/AlphabeticalIndex/AlphabeticalIndex/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace AlphabeticalIndex
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = "..\\..\\..\\text.txt";
+            string text;
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                text = reader.ReadToEnd();
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Text:");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(text);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nCodes:");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            AlphabeticalIndex alphabeticalIndex = new AlphabeticalIndex(text);
+            
+            foreach (var word in alphabeticalIndex.GetAlphabeticalIndex())
+            {
+                Console.Write($"({word.Item1}):");
+
+                for (var i = 0; i < word.Item2.Count; ++i)
+                {
+                    Console.Write($" {word.Item2[i]},");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ResetColor();
+        }
+    }
+}
